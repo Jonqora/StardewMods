ccaf57657888238260435356f808ca4ca45a7fa6
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-06-16 02:05:49 -0400

    [FishPreview] Version 1.0.0 is working!

--- FishPreview/ModConfig.cs
+++ FishPreview/ModConfig.cs
@@ -2,9 +2,10 @@
 namespace FishPreview
 {
     class ModConfig
-    {
-        public bool ShowUncaughtFishSpecies { get; set; } = false;
+    {   
         public bool ShowFishName { get; set; } = true;
         public string FishDisplayPosition { get; set; } = "UpperRight";
+        public bool ShowUncaughtFishSpecies { get; set; } = false;
+        public bool AlwaysShowLegendaryFish { get; set; } = false;
     }
 }
