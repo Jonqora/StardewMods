122036d6fd9e6a75ba35339add7373fa3009082d
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-06-18 12:44:51 -0400

    [ShowItemQuality] Added harmony prefix to Game1.addHUDMessage

--- ShowItemQuality/ModEntry.cs
+++ ShowItemQuality/ModEntry.cs
@@ -35,7 +35,13 @@ namespace ShowItemQuality
             // Apply the patch to show item quality when drawing in HUD
             harmony.Patch(
                 original: AccessTools.Method(typeof(StardewValley.HUDMessage), nameof(StardewValley.HUDMessage.draw)),
-                transpiler: new HarmonyMethod(AccessTools.Method(typeof(HUDPatch), nameof(HUDPatch.DrawMethodTranspiler)))
+                transpiler: new HarmonyMethod(AccessTools.Method(typeof(HUDPatch), nameof(HUDPatch.HUDMessageDraw_Transpiler)))
+            );
+
+            // Apply the patch to use most recent item in a stack to display HUD icon
+            harmony.Patch(
+                original: AccessTools.Method(typeof(StardewValley.Game1), nameof(StardewValley.Game1.addHUDMessage)),
+                prefix: new HarmonyMethod(AccessTools.Method(typeof(HUDPatch), nameof(HUDPatch.addHUDMessage_Prefix)))
             );
         }
         /*********
@@ -45,7 +51,7 @@ namespace ShowItemQuality
         internal class HUDPatch
         {
             /// <summary>Changes an argument in this.messageSubject.drawInMenu() to use StackDrawType.Draw instead of StackDrawType.Hide)</summary>
-            internal static IEnumerable<CodeInstruction> DrawMethodTranspiler(IEnumerable<CodeInstruction> instructions)
+            internal static IEnumerable<CodeInstruction> HUDMessageDraw_Transpiler(IEnumerable<CodeInstruction> instructions)
             {
                 var codes = new List<CodeInstruction>(instructions);
 
@@ -62,6 +68,56 @@ namespace ShowItemQuality
                     }
                 }
                 return codes.AsEnumerable();
+            }
+
+            /// <summary>When adding a new HUD message that stacks with a previous one, use the newest Message's messageSubject.</summary>
+            internal static bool addHUDMessage_Prefix(HUDMessage message)
+            {
+                try
+                {
+                    if (message.type != null || message.whatType != 0)
+                    {
+                        for (int index = 0; index < Game1.hudMessages.Count; ++index)
+                        {
+                            if (message.type != null && Game1.hudMessages[index].type != null && (Game1.hudMessages[index].type.Equals(message.type) && Game1.hudMessages[index].add == message.add))
+                            {
+                                //Game1.hudMessages[index].number = message.add ? Game1.hudMessages[index].number + message.number : Game1.hudMessages[index].number - message.number;
+
+                                //Altered code to affect and keep current message in the place of existing one
+                                message.number = message.add ? Game1.hudMessages[index].number + message.number : Game1.hudMessages[index].number - message.number;
+                                Game1.hudMessages.RemoveAt(index);
+                                Game1.hudMessages.Insert(index, message);
+
+                                Game1.hudMessages[index].timeLeft = 3500f;
+                                Game1.hudMessages[index].transparency = 1f;
+                                return false; // don't run original logic
+                            }
+                            if (message.whatType == Game1.hudMessages[index].whatType && message.whatType != 1 && (message.message != null && message.message.Equals(Game1.hudMessages[index].message)))
+                            {
+                                Game1.hudMessages[index].timeLeft = message.timeLeft;
+                                Game1.hudMessages[index].transparency = 1f;
+                                return false; // don't run original logic
+                            }
+                        }
+                    }
+                    Game1.hudMessages.Add(message);
+                    for (int index = Game1.hudMessages.Count - 1; index >= 0; --index)
+                    {
+                        if (Game1.hudMessages[index].noIcon)
+                        {
+                            HUDMessage hudMessage = Game1.hudMessages[index];
+                            Game1.hudMessages.RemoveAt(index);
+                            Game1.hudMessages.Add(hudMessage);
+                        }
+                    }
+
+                    return false; // don't run original logic
+                }
+                catch (Exception ex)
+                {
+                    ModMonitor.Log($"Failed in {nameof(addHUDMessage_Prefix)}:\n{ex}", LogLevel.Error);
+                    return true; // run original logic
+                }
             }
         }
     }
