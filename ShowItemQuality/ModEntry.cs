fb8c9032c6e2cea72f8e4d9b615456fd1d923b1f
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-06-19 16:30:54 -0400

    [ShowItemQuality] beta.2 change prefix to postfix

--- ShowItemQuality/ModEntry.cs
+++ ShowItemQuality/ModEntry.cs
@@ -41,7 +41,7 @@ namespace ShowItemQuality
             // Apply the patch to use most recent item in a stack to display HUD icon
             harmony.Patch(
                 original: AccessTools.Method(typeof(StardewValley.Game1), nameof(StardewValley.Game1.addHUDMessage)),
-                prefix: new HarmonyMethod(AccessTools.Method(typeof(HUDPatch), nameof(HUDPatch.addHUDMessage_Prefix)))
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(HUDPatch), nameof(HUDPatch.addHUDMessage_Postfix)))
             );
         }
         /*********
@@ -53,25 +53,32 @@ namespace ShowItemQuality
             /// <summary>Changes an argument in this.messageSubject.drawInMenu() to use StackDrawType.Draw instead of StackDrawType.Hide)</summary>
             internal static IEnumerable<CodeInstruction> HUDMessageDraw_Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                var codes = new List<CodeInstruction>(instructions);
-
-                for (int i = 0; i < codes.Count - 1; i++)
+                try
                 {
-                    // find Enum value of 0 (StackDrawType.Hide) loaded for the last argument of this.messageSubject.drawInMenu()
-                    if (codes[i].opcode == OpCodes.Ldc_I4_0 &&
-                        codes[i + 1].opcode == OpCodes.Callvirt &&
-                        codes[i + 1].operand.ToString() == "Void drawInMenu(Microsoft.Xna.Framework.Graphics.SpriteBatch, Microsoft.Xna.Framework.Vector2, Single, Single, Single, StardewValley.StackDrawType)")
-                    {
-                        // change to Enum value 1 (StackDrawType.Draw)
-                        codes[i].opcode = OpCodes.Ldc_I4_1;
-                        break;
-                    }
-                }
-                return codes.AsEnumerable();
-            }
+                    var codes = new List<CodeInstruction>(instructions);
 
-            /// <summary>When adding a new HUD message that stacks with a previous one, use the newest Message's messageSubject.</summary>
-            internal static bool addHUDMessage_Prefix(HUDMessage message)
+                    for (int i = 0; i < codes.Count - 1; i++)
+                    {
+                        // find Enum value of 0 (StackDrawType.Hide) loaded for the last argument of this.messageSubject.drawInMenu()
+                        if (codes[i].opcode == OpCodes.Ldc_I4_0 &&
+                            codes[i + 1].opcode == OpCodes.Callvirt &&
+                            codes[i + 1].operand.ToString() == "Void drawInMenu(Microsoft.Xna.Framework.Graphics.SpriteBatch, Microsoft.Xna.Framework.Vector2, Single, Single, Single, StardewValley.StackDrawType)")
+                        {
+                            // change to Enum value 1 (StackDrawType.Draw)
+                            codes[i].opcode = OpCodes.Ldc_I4_1;
+                            break;
+                        }
+                    }
+                    return codes.AsEnumerable();
+                }
+                catch (Exception ex)
+                {
+                    ModMonitor.Log($"Failed in {nameof(HUDMessageDraw_Transpiler)}:\n{ex}", LogLevel.Error);
+                    return instructions; // use original code
+                }
+            }
+            /// <summary>When adding an HUD message that stacks with a previous one, use the newest message to update messageSubject.</summary>
+            internal static void addHUDMessage_Postfix(HUDMessage message)
             {
                 try
                 {
@@ -81,42 +88,20 @@ namespace ShowItemQuality
                         {
                             if (message.type != null && Game1.hudMessages[index].type != null && (Game1.hudMessages[index].type.Equals(message.type) && Game1.hudMessages[index].add == message.add))
                             {
-                                //Game1.hudMessages[index].number = message.add ? Game1.hudMessages[index].number + message.number : Game1.hudMessages[index].number - message.number;
-
-                                //Altered code to affect and keep current message in the place of existing one
-                                message.number = message.add ? Game1.hudMessages[index].number + message.number : Game1.hudMessages[index].number - message.number;
+                                // Altered code to affect and keep current message in the place of existing one
+                                // Keep the updated stack number
+                                message.number = Game1.hudMessages[index].number; 
+                                // Replace the old HUDMessage with the new one, instead of keeping the old.
                                 Game1.hudMessages.RemoveAt(index);
                                 Game1.hudMessages.Insert(index, message);
-
-                                Game1.hudMessages[index].timeLeft = 3500f;
-                                Game1.hudMessages[index].transparency = 1f;
-                                return false; // don't run original logic
-                            }
-                            if (message.whatType == Game1.hudMessages[index].whatType && message.whatType != 1 && (message.message != null && message.message.Equals(Game1.hudMessages[index].message)))
-                            {
-                                Game1.hudMessages[index].timeLeft = message.timeLeft;
-                                Game1.hudMessages[index].transparency = 1f;
-                                return false; // don't run original logic
+                                return;
                             }
                         }
                     }
-                    Game1.hudMessages.Add(message);
-                    for (int index = Game1.hudMessages.Count - 1; index >= 0; --index)
-                    {
-                        if (Game1.hudMessages[index].noIcon)
-                        {
-                            HUDMessage hudMessage = Game1.hudMessages[index];
-                            Game1.hudMessages.RemoveAt(index);
-                            Game1.hudMessages.Add(hudMessage);
-                        }
-                    }
-
-                    return false; // don't run original logic
                 }
                 catch (Exception ex)
                 {
-                    ModMonitor.Log($"Failed in {nameof(addHUDMessage_Prefix)}:\n{ex}", LogLevel.Error);
-                    return true; // run original logic
+                    ModMonitor.Log($"Failed in {nameof(addHUDMessage_Postfix)}:\n{ex}", LogLevel.Error);
                 }
             }
         }
