023be3817710cf9c8dea5749acaf86f15ab1b9e5
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-07-07 18:41:56 -0400

    [AngryGrandpa] Refactor EventPatches to reuse same code in all postfixes

--- AngryGrandpa/src/Patches/EventPatches.cs
+++ AngryGrandpa/src/Patches/EventPatches.cs
@@ -45,7 +45,7 @@ namespace AngryGrandpa
 				original: AccessTools.Method(typeof(Event),
 					nameof(Event.skipEvent)),
 				postfix: new HarmonyMethod(typeof(EventPatches),
-					nameof(EventPatches.skipEvent_Postfix))
+					nameof(EventPatches.grandpaEvaluations_Postfix))
 			);
 		}
 
@@ -63,75 +63,54 @@ namespace AngryGrandpa
 			}
 		}
 		
-		public static void grandpaEvaluations_Postfix(GameLocation location)
+		public static void grandpaEvaluations_Postfix(Event __instance)
 		{
 			try
 			{
-				CheckWorldForStatueOfPerfection(); // Add reward flag to host if any pre-existing statue
-				foreach (int e in new List<int> { 2146991, 321777 }) // Remove candles event, re-evaluation flag
-				{
-					while (Game1.player.eventsSeen.Contains(e)) { Game1.player.eventsSeen.Remove(e); }
-				}
-				// Add a mail flag the FIRST time this mod is used for any evaluation. This activates bonus rewards.
-				if (!Game1.player.mailReceived.Contains("6324hasDoneModdedEvaluation"))
-				{
-					Game1.player.mailReceived.Add("6324hasDoneModdedEvaluation");
-				}
-
-				if (Config.ShowPointsTotal)
-				{
-					int grandpaScore = Utility.getGrandpaScore();
-					int maxScore = Config.GetMaxScore();
-					string displayText = i18n.Get("Event.cs.ShowGrandpaScore", new { grandpaScore, maxScore });
-					location.temporarySprites.Add(new TemporaryAnimatedSprite()
-					{
-						text = displayText,
-						local = true,
-						position = new Vector2((float)(Game1.viewport.Width / 2) - Game1.dialogueFont.MeasureString(displayText).X / 2f, (float)(Game1.tileSize / 2)), // was originally /4,  
-						color = Color.White,
-						interval = 20000f, // Lasts for 15 seconds -> changed to 20
-						layerDepth = 1f,
-						animationLength = 1,
-						initialParentTileIndex = 1,
-						currentParentTileIndex = 1,
-						totalNumberOfLoops = 1
-					});
-				}
-				Monitor.Log($"Ran patch for evaluation event: {nameof(grandpaEvaluations_Postfix)}", LogLevel.Debug);
-			}
-			catch (Exception ex)
-			{
-				Monitor.Log($"Failed in {nameof(grandpaEvaluations_Postfix)}:\n{ex}",
-					LogLevel.Error);
-			}
-		}
-
-		public static void skipEvent_Postfix(Event __instance)
-		{
-			try
-			{
-				switch(__instance.id)
+				switch (__instance.id) // Check which event this is... we're patching skipEvent and don't want to affect all.
 				{
 					case 558291: // Initial
 					case 558292: // Reevaluation
+
 						CheckWorldForStatueOfPerfection(); // Add reward flag to host if any pre-existing statue
 						foreach (int e in new List<int> { 2146991, 321777 }) // Remove candles event, re-evaluation flag
 						{
 							while (Game1.player.eventsSeen.Contains(e)) { Game1.player.eventsSeen.Remove(e); }
 						}
+						// Add a mail flag the FIRST time this mod is used for any evaluation. 
 						if (!Game1.player.mailReceived.Contains("6324hasDoneModdedEvaluation"))
 						{
-							Game1.player.mailReceived.Add("6324hasDoneModdedEvaluation"); // Activate bonus rewards
+							Game1.player.mailReceived.Add("6324hasDoneModdedEvaluation"); // This activates bonus rewards if enabled.
 						}
+
+						if (Config.ShowPointsTotal && !__instance.skipped) // Don't show if disabled in config or the event was skipped
+						{
+							int grandpaScore = Utility.getGrandpaScore();
+							int maxScore = Config.GetMaxScore();
+							string displayText = i18n.Get("Event.cs.ShowGrandpaScore", new { grandpaScore, maxScore });
+							Game1.currentLocation.temporarySprites.Add(new TemporaryAnimatedSprite()
+							{
+								text = displayText,
+								local = true,
+								position = new Vector2((float)(Game1.viewport.Width / 2) - Game1.dialogueFont.MeasureString(displayText).X / 2f, (float)(Game1.tileSize / 2)), // was originally /4,  
+								color = Color.White,
+								interval = 20000f, // Lasts for 15 seconds -> changed to 20
+								layerDepth = 1f,
+								animationLength = 1,
+								initialParentTileIndex = 1,
+								currentParentTileIndex = 1,
+								totalNumberOfLoops = 1
+							});
+						}
+						Monitor.Log($"Ran patch for evaluation event: {nameof(grandpaEvaluations_Postfix)}", LogLevel.Debug);
 						break;
 					default:
 						break;
 				}
-				Monitor.Log($"Ran patch for skip event logic: {nameof(skipEvent_Postfix)}", LogLevel.Debug);
 			}
 			catch (Exception ex)
 			{
-				Monitor.Log($"Failed in {nameof(skipEvent_Postfix)}:\n{ex}",
+				Monitor.Log($"Failed in {nameof(grandpaEvaluations_Postfix)}:\n{ex}",
 					LogLevel.Error);
 			}
 		}
