3e223afbfb96e9de4dfd674335d374cc75a25957
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-07-07 13:56:21 -0400

    [AngryGrandpa] Use while loops to iterate flags in cmdResetEvaluation
    If applied, this should solve the bug of event or mail flags not
    being properly removed from save data on Android.

--- AngryGrandpa/src/ModEntry.cs
+++ AngryGrandpa/src/ModEntry.cs
@@ -187,12 +187,20 @@ namespace AngryGrandpa
                 {
                     throw new Exception("Cannot remove event flags without an active save.");
                 }
-                Game1.player.eventsSeen.Remove(558291); // Initial evaluation
-                Game1.player.eventsSeen.Remove(558292); // Re-evaluation
-                // Game1.player.eventsSeen.Remove(2146991); // Candle lighting (this is now removed by command_grandpaEvaluation postfix)
-                Game1.player.eventsSeen.Remove(321777); // Evaluation request
+                var eventsToRemove = new List<int>
+                {
+                    558291, 558292, 321777, // Initial eval, Re-eval, and Evaluation request
+                };
+                foreach (int e in eventsToRemove)
+                {
+                    while (Game1.player.eventsSeen.Contains(e)) { Game1.player.eventsSeen.Remove(e); }
+                }
+                // Game1.player.eventsSeen.Remove(2146991); // Candles (removed instead by command_grandpaEvaluation postfix)
                 Game1.getFarm().hasSeenGrandpaNote = false; // Seen the note on the shrine
-                Game1.player.mailReceived.Remove("grandpaPerfect"); // Received the statue of perfection
+                while (Game1.player.mailReceived.Contains("grandpaPerfect")) // Received the statue of perfection
+                { 
+                    Game1.player.mailReceived.Remove("grandpaPerfect"); 
+                } 
                 Game1.getFarm().grandpaScore.Value = 0; // Reset grandpaScore
                 FarmPatches.RemoveCandlesticks(Game1.getFarm()); // Removes all candlesticks (not flames).
                 Game1.getFarm().removeTemporarySpritesWithIDLocal(6666f); // Removes candle flames.
@@ -203,9 +211,9 @@ namespace AngryGrandpa
                     "6324bonusRewardsEnabled", "6324reward2candles", "6324reward3candles", // Old, outdated flags
                     "6324grandpaNoteMail", "6324reward1candle", "6324reward2candle", "6324reward3candle", "6324reward4candle", "6324hasDoneModdedEvaluation", // Current used flags
                 };
-                foreach (string flag in Game1.player.mailReceived)
+                foreach (string flag in flagsToRemove)
                 {
-                    if (flagsToRemove.Contains(flag)) { Game1.player.mailReceived.Remove(flag); }
+                    while (Game1.player.mailReceived.Contains(flag)) { Game1.player.mailReceived.Remove(flag); }
                 }
 
                 if (!Game1.player.eventsSeen.Contains(2146991))
