01cbec6d76798d934116c6b9f4dfb71c8e1e23ec
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-07-07 13:38:41 -0400

    [ExtendedEmotes] Initial commit (not a functional build yet)

---
+++ ExtendedEmotes/TileSheetEditor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace ExtendedEmotes
+{
+	internal class TileSheetEditor : IAssetEditor
+	{
+		protected static IModHelper Helper => ModEntry.Instance.Helper;
+		protected static IMonitor Monitor => ModEntry.Instance.Monitor;
+		//protected static ModConfig Config => ModConfig.Instance;
+
+		//protected static ITranslationHelper i18n = Helper.Translation;
+
+		public bool CanEdit<_T> (IAssetInfo asset)
+		{
+			return asset.AssetNameEquals($"TileSheets\\Emotes");
+		}
+
+		public void Edit<_T> (IAssetData asset)
+		{
+			var editor = asset.AsImage();
+			Texture2D sourceImage = Helper.Content.Load<Texture2D>("assets\\Grandpa.png", ContentSource.ModFolder);
+
+			editor.ExtendImage(minWidth: 128, minHeight: 384);
+			editor.PatchImage(sourceImage);
+
+			Monitor.Log($"Edited {asset.AssetName}", LogLevel.Debug);
+		}
+	}
+}
