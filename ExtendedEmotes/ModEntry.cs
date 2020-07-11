74b6536ab5961f765a76240591f46d43605a1567
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-07-11 15:36:27 -0400

    [ExtendedEmotes] Edit to be a functional csproj

--- ExtendedEmotes/ModEntry.cs
+++ ExtendedEmotes/ModEntry.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Harmony;
 
 namespace ExtendedEmotes
 {
-    public class ModEntry
+    public class ModEntry : Mod
     {
+        /*********
+        ** Fields
+        *********/
+        internal static ModEntry Instance { get; private set; }
+        internal HarmonyInstance Harmony { get; private set; }
+
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            // Make resources available.
+            Instance = this;
+
+            // Apply Harmony patches.
+            Harmony = HarmonyInstance.Create(ModManifest.UniqueID);
+            EventCommandPatches.Apply();
+
+            // Listen for game events.
+            helper.Events.GameLoop.GameLaunched += this.onGameLaunched;
+
+            // Set up portrait asset editor. This one is added early since it never changes.
+            helper.Content.AssetEditors.Add(new TileSheetEditor());
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /****
+        ** Event handlers
+        ****/
+        /// <summary>
+        /// Called after the game is launched, before the first update tick.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void onGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            
+        }
     }
 }
--- ExtendedEmotes/TileSheetEditor.cs
+++ ExtendedEmotes/TileSheetEditor.cs
@@ -6,17 +6,28 @@ namespace ExtendedEmotes
 {
 	internal class TileSheetEditor : IAssetEditor
 	{
+		/*********
+        ** Accessors
+        *********/
 		protected static IModHelper Helper => ModEntry.Instance.Helper;
 		protected static IMonitor Monitor => ModEntry.Instance.Monitor;
-		//protected static ModConfig Config => ModConfig.Instance;
 
-		//protected static ITranslationHelper i18n = Helper.Translation;
 
+		/*********
+        ** Public methods
+        *********/
+		/// <summary>Get whether this instance can edit the given asset.</summary>
+		/// <typeparam name="_T">The asset Type.</typeparam>
+		/// <param name="asset">Basic metadata about the asset being loaded.</param>
+		/// <returns>true for asset TileSheets\Emotes, false otherwise</returns>
 		public bool CanEdit<_T> (IAssetInfo asset)
 		{
 			return asset.AssetNameEquals($"TileSheets\\Emotes");
 		}
 
+		/// <summary>Extend the TileSheets\Emotes spritesheet downwards to add new images onto the bottom.</summary>
+		/// <typeparam name="_T">The asset Type</typeparam>
+		/// <param name="asset">A helper which encapsulates metadata about an asset and enables changes to it</param>
 		public void Edit<_T> (IAssetData asset)
 		{
 			var editor = asset.AsImage();
