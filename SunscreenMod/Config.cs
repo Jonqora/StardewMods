60669c195ebcb765ff00ce2230b21eb16d1dddc0
Ellen Lloyd <ellen.lloyd@usask.ca>
2020-08-11 02:32:53 -0400

    [Sunscreen] Initial template files and new JA item packs

---
+++ SunscreenMod/Config.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+
+namespace SunscreenMod
+{
+    class Config
+    {
+        public SButton debugKey { get; set; }
+
+        public Config()
+        {
+            debugKey = SButton.J;
+        }
+    }
+}
